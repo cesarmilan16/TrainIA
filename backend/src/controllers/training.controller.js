09168877d4c7e966a89d1579c8f9e72792df2839
cesarmilan16 <cesar3bfermi@gmail.com>
2026-03-04 10:20:56 +0100

    creamos paginación

--- backend/src/controllers/training.controller.js
+++ backend/src/controllers/training.controller.js
@@ -68,7 +68,7 @@ export const generateTrainingController = async (req, res) => {
             id,
             ...training
         });
-        
+
     } catch (error) {
         console.error("ERROR REAL:", error);
         return res.status(500).json({
@@ -77,10 +77,84 @@ export const generateTrainingController = async (req, res) => {
     }
 };
 
-export const getTrainingByIdController = async (req, res) => {
-    const id = req.params.id
+export const getTrainingByIdController = (req, res) => {
+    const idParams = req.params.id
 
-    const result = db.prepare("SELECT * FROM trainings WHERE id = ?").get(id);
+    const result = db.prepare("SELECT * FROM trainings WHERE id = ?").get(idParams);
 
-    res.json(result);
+    if (!result) {
+        return res.status(404).json({
+            error: "Not found"
+        })
+    }
+
+    const { id, goal, days_per_week, training_split, experience_level, equipment, status, created_at } = result;
+
+    const training_json = JSON.parse(result.training_json);
+
+    res.status(200).json({
+        id: id,
+        status: status,
+        createdAt: created_at,
+        input: {
+            goal: goal,
+            daysPerWeek: days_per_week,
+            trainingSplit: training_split,
+            experienceLevel: experience_level,
+            equipment: equipment
+        },
+        training: training_json
+    });
+};
+
+export const getTrainingController = (req, res) => {
+    const { page, limit } = req.query;
+
+    const pageNumber = Math.max(1, Number(page) || 1);
+    const limitNumber = Math.min(50, Math.max(1, Number(limit) || 10));
+
+    const offset = (pageNumber - 1) * limitNumber;
+
+    const result = db.prepare(`
+        SELECT id, goal, days_per_week, training_split, experience_level, equipment, status, created_at 
+        FROM trainings 
+        ORDER BY created_at DESC
+        LIMIT ?
+        OFFSET ?
+        `).all(limitNumber, offset);
+
+    const formated = result.map(row => {
+        return {
+            id: row.id,
+            goal: row.goal,
+            daysPerWeek: row.days_per_week,
+            trainingSplit: row.training_split,
+            experienceLevel: row.experience_level,
+            equipment: row.equipment,
+            status: row.status,
+            createdAt: row.created_at
+        }
+    });
+
+    const resultTotal = db.prepare(`
+        SELECT COUNT(*) as total FROM trainings
+        `).get();
+
+    console.log(resultTotal);
+    
+    
+    const total = resultTotal.total;
+    
+
+    const totalPages = Math.ceil(total / limitNumber);
+
+    res.status(200).json({
+        data: formated,
+        pagination: {
+            page: pageNumber,
+            limit: limitNumber,
+            total: total,
+            totalPages: totalPages
+        }
+    });
 };
--- backend/src/routes/training.routes.js
+++ backend/src/routes/training.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { generateTrainingController, getTrainingByIdController } from "../controllers/training.controller.js";
+import { generateTrainingController, getTrainingByIdController, getTrainingController } from "../controllers/training.controller.js";
 
 const router = express.Router();
 
 router.post("/generate", generateTrainingController);
-router.get("/:id", getTrainingByIdController)
+router.get("/:id", getTrainingByIdController);
+router.get("/", getTrainingController);
 
 export default router;
