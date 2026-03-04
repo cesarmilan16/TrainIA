be87bc4e9c1ca81f7f3eb49a8f2115d34c983bf2
cesarmilan16 <cesar3bfermi@gmail.com>
2026-03-04 11:57:37 +0100

    añadimos la ruta PUT

--- backend/src/controllers/training.controller.js
+++ backend/src/controllers/training.controller.js
@@ -35,46 +35,61 @@ export const generateTrainingController = async (req, res) => {
         });
     }
 
-    try {
-        const training = await generateTraining(validatedData);
+    const id = uuidv4();
 
-        const id = uuidv4();
+    db.prepare(`
+    INSERT INTO trainings (
+        id,
+        goal,
+        days_per_week,
+        training_split,
+        experience_level,
+        equipment,
+        status,
+        training_json,
+        created_at
+    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+    `).run(
+        id,
+        validatedData.goal,
+        validatedData.daysPerWeek,
+        validatedData.trainingSplit,
+        validatedData.experienceLevel,
+        validatedData.equipment,
+        "GENERATING",
+        null,
+        new Date().toISOString()
+    );
 
-        db.prepare(`
-        INSERT INTO trainings (
-            id,
-            goal,
-            days_per_week,
-            training_split,
-            experience_level,
-            equipment,
-            status,
-            training_json,
-            created_at
-        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
-        `).run(
-            id,
-            validatedData.goal,
-            validatedData.daysPerWeek,
-            validatedData.trainingSplit,
-            validatedData.experienceLevel,
-            validatedData.equipment,
-            "COMPLETED",
-            JSON.stringify(training),
-            new Date().toISOString()
-        );
-
-        return res.status(200).json({
-            id,
-            ...training
+    generateTraining(validatedData)
+        .then(training => {
+            db.prepare(`
+                UPDATE trainings
+                SET status = ?, training_json = ?
+                WHERE id = ?
+            `).run(
+                "COMPLETED",
+                JSON.stringify(training),
+                id
+            );
+        })
+        .catch(error => {
+            db.prepare(`
+                UPDATE trainings
+                SET status = ?
+                WHERE id = ?
+            `).run(
+                "FAILED",
+                id
+            );
+            console.error("Background generation error:", error);
         });
 
-    } catch (error) {
-        console.error("ERROR REAL:", error);
-        return res.status(500).json({
-            error: "Failed to generate training"
-        });
-    }
+    res
+        .status(202)
+        .location(`/api/trainings/${id}`)
+        .json({ id, status: "GENERATING" });
+
 };
 
 export const getTrainingByIdController = (req, res) => {
@@ -90,9 +105,26 @@ export const getTrainingByIdController = (req, res) => {
 
     const { id, goal, days_per_week, training_split, experience_level, equipment, status, created_at } = result;
 
-    const training_json = JSON.parse(result.training_json);
+    const training = result.training_json
+        ? JSON.parse(result.training_json)
+        : null;
 
-    res.status(200).json({
+    if (status !== "COMPLETED") {
+        return res.status(200).json({
+            id: id,
+            status: status,
+            createdAt: created_at,
+            input: {
+                goal: goal,
+                daysPerWeek: days_per_week,
+                trainingSplit: training_split,
+                experienceLevel: experience_level,
+                equipment: equipment
+            },
+        });
+    };
+
+    return res.status(200).json({
         id: id,
         status: status,
         createdAt: created_at,
@@ -103,7 +135,7 @@ export const getTrainingByIdController = (req, res) => {
             experienceLevel: experience_level,
             equipment: equipment
         },
-        training: training_json
+        training: training
     });
 };
 
@@ -141,10 +173,10 @@ export const getTrainingController = (req, res) => {
         `).get();
 
     console.log(resultTotal);
-    
-    
+
+
     const total = resultTotal.total;
-    
+
 
     const totalPages = Math.ceil(total / limitNumber);
 
@@ -157,4 +189,72 @@ export const getTrainingController = (req, res) => {
             totalPages: totalPages
         }
     });
+};
+
+export const regenerateTrainingController = (req, res) => {
+    const idParams = req.params.id;
+
+    const result = db.prepare(`
+        SELECT * FROM trainings
+        WHERE id = ?
+        `).get(idParams);
+
+    if (!result) {
+        return res.status(404).json({
+            error: "Entrenamiento no encontrado"
+        });
+    };
+
+    if (result.status === "GENERATING") {
+        return res.status(409).json({
+            error: "El entrenamiento se esta generando"
+        });
+    };
+
+    const inputData = {
+        goal: result.goal,
+        daysPerWeek: result.days_per_week,
+        trainingSplit: result.training_split,
+        experienceLevel: result.experience_level,
+        equipment: result.equipment
+    };
+
+    db.prepare(`
+        UPDATE trainings
+        SET status = ?, training_json = ?
+        WHERE id = ?
+        `).run(
+        "GENERATING",
+        null,
+        idParams
+    );
+
+    generateTraining(inputData)
+        .then(training => {
+            db.prepare(`
+                UPDATE trainings
+                SET status = ?, training_json = ?
+                WHERE id = ?
+            `).run(
+                "COMPLETED",
+                JSON.stringify(training),
+                idParams
+            );
+        })
+        .catch(error => {
+            db.prepare(`
+                UPDATE trainings
+                SET status = ?
+                WHERE id = ?
+            `).run(
+                "FAILED",
+                idParams
+            );
+            console.error("Background generation error:", error);
+        });
+
+    res.status(202).json({
+        id: idParams,
+        status: "GENERATING"
+    })
 };
--- backend/src/routes/training.routes.js
+++ backend/src/routes/training.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { generateTrainingController, getTrainingByIdController, getTrainingController } from "../controllers/training.controller.js";
+import { generateTrainingController, getTrainingByIdController, getTrainingController, regenerateTrainingController } from "../controllers/training.controller.js";
 
 const router = express.Router();
 
 router.post("/generate", generateTrainingController);
 router.get("/:id", getTrainingByIdController);
 router.get("/", getTrainingController);
+router.put("/:id", regenerateTrainingController);
 
 export default router;
