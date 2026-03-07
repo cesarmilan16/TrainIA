431ebef87c3fea369eb39f7ff6771dc96f3e3709
cesarmilan16 <cesar3bfermi@gmail.com>
2026-03-07 04:47:58 +0100

    Añadir inyecciones de dependencias y configuración de formularios reactivos en el componente TrainingForm

--- frontend/src/app/features/trainings/components/training-form/training-form.ts
+++ frontend/src/app/features/trainings/components/training-form/training-form.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { TrainingSplit, ExperienceLevel, Equipment } from '../../../../core/trainings/training-api.models';
+import { TrainingApiService } from '../../../../core/trainings/training-api.service';
 
 @Component({
   selector: 'app-training-form',
-  imports: [],
+  imports: [ ReactiveFormsModule ],
   templateUrl: './training-form.html',
   styleUrl: './training-form.css',
 })
 export class TrainingForm {
+  private readonly trainingApi = inject(TrainingApiService);
+  private readonly fb = inject(FormBuilder);
+
 
 }
